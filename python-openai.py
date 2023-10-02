b0b3bd793696172a44810501d723a2ea71236427
lillitaz <lilli.weitzer@gmail.com>
2023-10-02 12:39:25 +0200

    python-openai.py argparser for script

--- python-openai.py
+++ python-openai.py
@@ -1,8 +1,14 @@
 import requests
+import argparse
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
+parser = argparse.ArgumentParser()
+parser.add_argument("prompt", help="The prompt to send to the API")
+parser.add_argument("file_name", help="Name of the file to save Script")
+args = parser.parse_args()
 
 api_endpoint = "https://api.openai.com/v1/completions"
 api_key = os.getenv("OPENAI_API_KEY")
@@ -13,15 +19,19 @@ request_headers = {
 }
 
 request_data = {
-    "model": "davinci-002",
-    "prompt": "Write python script for hello world",
-    "max_tokens": 150,
-    "temperature": 0.7
+    "model": "text-davinci-003",
+    "prompt": f"Write python script to {args.prompt}. Provide only code, no text.",
+    "max_tokens": 500,
+    "temperature": 0.6
 
 }
 response = requests.post(api_endpoint, headers=request_headers, json=request_data)
 
 if response.status_code == 200:
     print(response.json())
+    response_text = response.json()["choices"][0]["text"]
+    with open(args.file_name, "w") as file:
+        file.write(response_text)
+
 else:
     print(f"Request failed {response.status_code}: {response.json()}")
